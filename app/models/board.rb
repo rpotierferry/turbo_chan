1dd931c529dc9911c15bfef19a61a82722c567ad
Romain Potier-Ferry <romainpotierferry@gmail.com>
2024-05-01 18:30:05 +0200

    add validations

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,6 +2,9 @@ class Board < ApplicationRecord
   before_create :assign_b_id
   has_many :board_threads, dependent: :destroy
 
+  validates :name, :handle, :category, :description, presence: true
+  validates :handle, uniqueness: true, format: { with: %r{/\w+/} }
+
   private
 
   def assign_b_id
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,22 @@
 class Post < ApplicationRecord
   before_create :assign_p_id
   belongs_to :board_thread, counter_cache: true
+
   has_many :votes, dependent: :destroy
   has_one_attached :photo
+
+  validates :title, presence: true, if: :op?, length: { minimum: 6, maximum: 64 }
+  validates :content, presence: true, if: :op?, length: { maximum: 256 }
+  validates :photo, presence: true, if: :op?
 
   private
 
   def assign_p_id
     max_id = Post.maximum(:p_id) || 0
     self.p_id = max_id + 1
+  end
+
+  def op?
+    self.is_op
   end
 end
